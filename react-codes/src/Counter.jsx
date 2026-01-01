cd21db4592149b460aa68dc4727f44550994c6a4
Raj Ranjan <raj.ranjan9178@gmail.com>
2026-01-01 12:19:02 +0530

    Practiced a Counter Mini Project

---
+++ react-codes/src/Counter.jsx
@@ -0,0 +1,29 @@
+import { useState } from "react";
+
+function Counter() {
+
+    let [count, setCount] = useState(10);
+
+    const increment = () => {
+        if(count < 20) {
+            setCount(count + 1)
+        }
+    }
+
+    const decrement = () => {
+        if(count > 0) {
+            setCount(count - 1);
+        }
+    }
+
+    return (
+        <>
+        <h1>Count: {count}</h1>
+        <button onClick={increment}>Increment</button>
+        <button onClick={decrement}>Decrement</button>
+        </>
+    )
+}
+
+
+export default Counter;
